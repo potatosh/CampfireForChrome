1ae18172361ab1a8b26b360f5b1583288d635b35
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2012-01-06 20:19:50 +0900

    CSSを2つに分けて、途中までモックアップした

--- scripts/camping.js
+++ scripts/camping.js
@@ -1,1 +1,4 @@
-$("#room_list").mCustomScrollbar("horizontal",200,"easeOutCirc",1.05,"auto","yes","no",0);
+$('img').load(function() {
+  $("#room_list").mCustomScrollbar("horizontal", 100, "easeOutCirc", 1, "auto", "yes", "no", 0);
+  $("#message_list").mCustomScrollbar("vertical", 100, "easeOutCirc", 1, "auto", "yes", "yes", 5);
+});
