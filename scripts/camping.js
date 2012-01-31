95e0a064acb8eeab8fd52fda576d58c677f71e15
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2012-01-31 18:44:09 +0900

    ルームボタンを表示できるようになったよ

--- scripts/camping.js
+++ scripts/camping.js
@@ -1,4 +1,36 @@
-$('img').load(function() {
-  $("#room_list").mCustomScrollbar("horizontal", 100, "easeOutCirc", 1, "auto", "yes", "no", 0);
-  $("#message_list").mCustomScrollbar("vertical", 100, "easeOutCirc", 1, "auto", "yes", "yes", 5);
-});
+if(localStorage["token"]) {
+  campfireAjax(
+    "GET",
+    "/rooms.xml",
+    {},
+    function(data) {
+      var rooms = $(data).find("rooms room");
+      $(rooms).each(function() {
+        var jqe = $(this);
+        $("#room_list .content").append(
+          $("<div>").attr("id", "room_" + jqe.find("id").text())
+                    .addClass("roomButton")
+                    .text(jqe.find("name").text())
+        );
+      });
+      $("#room_list").mCustomScrollbar("horizontal", 100, "easeOutCirc", 1, "auto", "yes", "no", 0);
+      $("#message_list").mCustomScrollbar("vertical", 100, "easeOutCirc", 1, "auto", "yes", "yes", 10);
+    },
+    function(xhr) {
+      $("#room_list .content").html("Authorize failed.").fadeIn(300);
+    }
+  );
+} else {
+}
+
+function campfireAjax(type, path, data, success, error) {
+  $.ajax({
+    url: "https://" + localStorage["account"] + ".campfirenow.com" + path,
+    username: localStorage["token"],
+    password: "x",
+    type: type,
+    data: data,
+    success: success,
+    error: error
+  });
+}
